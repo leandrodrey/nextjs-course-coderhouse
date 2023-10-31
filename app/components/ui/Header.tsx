2dcd6151478eb4173c11f282292206005a729897
Leandro Rey <leandrodrey@gmail.com>
2023-10-31 14:50:10 -0300

    Header component added, first approach

---
+++ app/components/ui/Header.tsx
@@ -0,0 +1,13 @@
+import {FC} from 'react';
+
+const Header:FC = () => {
+    return (
+        <>
+            <header className="w-full bg-[#0f0f0f] py-5 shadow">
+                <h1 className="text-base font-bold"> Next.js + Tailwind CSS</h1>
+            </header>
+        </>
+    )
+}
+
+export default Header;
