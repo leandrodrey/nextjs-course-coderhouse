521b205c0a55f5dfbc9bf45066d351bea05d81b5
Leandro Rey <leandrodrey@gmail.com>
2023-11-01 20:15:46 -0300

    Products from items.json

--- app/components/ui/ProductContainer.tsx
+++ app/components/ui/ProductContainer.tsx
@@ -4,18 +4,11 @@ import data from "@/app/items.json";
 
 const ProductContainer:FC = () => {
     return (
-        <>
-            <div className="flex-1 p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                <ProductCard name="Producto 1" image="" description="Lorem ipsum sit dolor amet" />
-                <ProductCard name="Producto 1" image="" description="Lorem ipsum sit dolor amet" />
-                <ProductCard name="Producto 1" image="" description="Lorem ipsum sit dolor amet" />
-                <ProductCard name="Producto 1" image="" description="Lorem ipsum sit dolor amet" />
-                <ProductCard name="Producto 1" image="" description="Lorem ipsum sit dolor amet" />
-                <ProductCard name="Producto 1" image="" description="Lorem ipsum sit dolor amet" />
-                <ProductCard name="Producto 1" image="" description="Lorem ipsum sit dolor amet" />
-                <ProductCard name="Producto 1" image="" description="Lorem ipsum sit dolor amet" />
-            </div>
-        </>
+        <div className="flex-1 p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {data.map((item) => (
+                <ProductCard key={item.id} name={item.title} image={item.image} description={item.description} />
+            ))}
+        </div>
     )
 }
 
