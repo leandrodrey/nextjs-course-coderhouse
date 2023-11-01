8fffc59f68b5b79776505584a4f878b40a16354d
Leandro Rey <leandrodrey@gmail.com>
2023-11-01 15:33:35 -0300

    Fixed brackets

--- app/components/ui/ProductCard.tsx
+++ app/components/ui/ProductCard.tsx
@@ -10,9 +10,9 @@ const ProductCard:FC<ProductCardProps> = ({name, image , description }) => {
     return (
         <>
             <div className="bg-white shadow rounded-lg p-4">
-                <img alt={name} src={{ image }} />
-                <h2 className="text-lg mb-2 text-blue-400">{ name }</h2>
-                <p className="text-gray-700 mb-4 text-sm">{ description }</p>
+                <img alt={name} src={image} />
+                <h2 className="text-lg mb-2 text-blue-400">{name}</h2>
+                <p className="text-gray-700 mb-4 text-sm">{description}</p>
                 <button className="bg-blue-500 text-white text-sm py-1 px-2 rounded hover:bg-blue-600">Comprar</button>
             </div>
         </>
