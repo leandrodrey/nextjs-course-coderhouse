6b3f73ae196079acaa1a4107fa4b9fe23d725072
Leandro Rey <leandrodrey@gmail.com>
2023-11-06 23:48:08 -0300

    Updated image in detail

--- app/components/ui/ProductsContainer.tsx
+++ app/components/ui/ProductsContainer.tsx
@@ -9,7 +9,7 @@ interface ProductsContainerProps {
 const ProductsContainer: FC<ProductsContainerProps> = ({products}) => {
 
     return (
-        <div className="flex-1 p-4 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
+        <div className="flex-1 p-4 grid gap-4">
             {products.map((product: Product) => (
                 <ProductCard key={product.id} product={product}/>
             ))}
