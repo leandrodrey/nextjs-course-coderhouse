4829c116a381009e0e7f6554fbc1d0811f4aa447
Leandro Rey <leandrodrey@gmail.com>
2023-11-06 21:37:23 -0300

    Arrow changed

--- app/components/ui/NavBar.tsx
+++ app/components/ui/NavBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import {FC, useState} from "react";
-import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
+import ArrowBackIosOutlinedIcon from '@mui/icons-material/ArrowBackIosOutlined';
 import SocialIcons from "@/app/components/ui/SocialIcons";
 import Menu from "@/app/components/ui/Menu";
 
@@ -12,7 +12,7 @@ const NavBar: FC = () => {
         <>
             <div className={` ${open ? "w-60" : "w-20 "} flex flex-col justify-between bg-[#0f0f0f] h-screen p-5 pt-8 relative duration-300`}>
                 <div>
-                    <ArrowCircleLeftIcon className={`text-blue-300 absolute cursor-pointer -right-3 top-9 w-7 ${!open && "rotate-180"}`} onClick={() => setOpen(!open)}/>
+                    <ArrowBackIosOutlinedIcon className={`text-blue-300 absolute cursor-pointer -right-3 top-10 w-7 ${!open && "rotate-180"}`} onClick={() => setOpen(!open)}/>
                     <div className="flex flex-col gap-x-4 items-center">
                         <img
                             alt={"logo"}
