28061a1b04ac2efe12423f0507645cd59bceb9ec
Leandro Rey <leandrodrey@gmail.com>
2023-11-09 17:43:50 -0300

    Added button component

--- app/components/ui/Button.tsx
+++ app/components/ui/Button.tsx
@@ -6,7 +6,7 @@ interface ButtonProps {
 
 const Button: FC<ButtonProps> = ({text}) => {
     return (
-        <button className="bg-blue-300 text-black text-sm py-2 px-4 rounded transition duration-300 hover:bg-blue-600 hover:text-white">
+        <button className="bg-blue-300 text-black text-sm py-1 px-2 rounded transition duration-300 hover:bg-blue-600 shadow hover:text-white">
             {text}
         </button>
     )
