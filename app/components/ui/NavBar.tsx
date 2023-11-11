ac50de4504fbc545d0370f847890f5f27dce7606
Leandro Rey <leandrodrey@gmail.com>
2023-11-11 17:03:20 -0300

    Logo from cloudinary

--- app/components/ui/NavBar.tsx
+++ app/components/ui/NavBar.tsx
@@ -1,12 +1,16 @@
 "use client";
 import {FC, useState} from "react";
+import Link from "next/link";
 import ArrowBackIosOutlinedIcon from '@mui/icons-material/ArrowBackIosOutlined';
+import CloudinaryImage from "@/app/services/CloudinaryImage";
 import SocialIcons from "@/app/components/ui/SocialIcons";
 import Menu from "@/app/components/ui/Menu";
 
 const NavBar: FC = () => {
 
     const [open, setOpen] = useState(false);
+
+    const logoImage = CloudinaryImage("/gamebazar/logo.png");
 
     return (
         <>
@@ -15,11 +19,13 @@ const NavBar: FC = () => {
                     <div>
                         <ArrowBackIosOutlinedIcon className={`text-blue-300 absolute cursor-pointer -right-3 top-10 w-7 ${!open && "rotate-180"}`} onClick={() => setOpen(!open)}/>
                         <div className="flex flex-col gap-x-4 items-center">
-                            <img
-                                alt={"logo"}
-                                src="/logo.png"
-                                className={`cursor-pointer duration-500 ${open && "rotate-[360deg]"}`}
-                            />
+                            <Link href="/">
+                                <img
+                                    alt={"logo"}
+                                    src={logoImage}
+                                    className={`cursor-pointer duration-500 ${open && "rotate-[360deg]"}`}
+                                />
+                            </Link>
                             <h1 className={`py-2 text-white origin-left font-medium text-2xl duration-200 ${!open && "scale-0"}`}>
                                 Game Bazar
                             </h1>
