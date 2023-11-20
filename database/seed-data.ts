e1274b8cc2ab7a06876d2fdcbad3899f0fe14e93
Leandro Rey <leandrodrey@gmail.com>
2023-11-20 18:14:07 -0300

    Api route to populate data

---
+++ database/seed-data.ts
@@ -0,0 +1,126 @@
+export const seedData = {
+    products: [
+        {
+            "title": "Elden Ring",
+            "description": "Elden Ring, an action RPG set in a vast fantasy world, is the collaborative creation of Hidetaka Miyazaki and George R. R. Martin. The game offers a unique experience combining deep lore, intense combat, and an expansive world full of rich landscapes and mysterious dungeons. Players are invited to explore, battle fearsome creatures, and uncover the mysteries of the Elden Ring.",
+            "image": "xfomwfx5y1ktqma2vszw",
+            "price": 59.99,
+            "categoryId": 1,
+            "categoryName": "RPG"
+        },
+        {
+            "title": "Cyberpunk 2077",
+            "description": "Cyberpunk 2077 is a story-driven, open-world RPG set in the dark and vibrant future of Night City. Developed by CD Projekt Red, the game immerses players in a world of cybernetic enhancements, fast-paced action, and intricate storylines. Players take on the role of V, a mercenary outlaw, and navigate through a narrative shaped by their choices, exploring the depths of corporate conspiracies and urban warfare.",
+            "image": "zif2rpcnmcpmqixuelil",
+            "price": 49.99,
+            "categoryId": 1,
+            "categoryName": "RPG"
+        },
+        {
+            "title": "Valheim",
+            "description": "Valheim is a brutal exploration and survival game for 1-10 players, set in a procedurally-generated purgatory inspired by Viking culture. Players must craft, build, and conquer their way through a hostile land, battling not only the elements but also a variety of mythical creatures. The game's vast open world challenges players to explore its dense forests, towering mountains, and treacherous seas, all while surviving and thriving in a harsh and unforgiving environment.",
+            "image": "kbh8a8jmzm1mmknsmi4h",
+            "price": 19.99,
+            "categoryId": 2,
+            "categoryName": "Survival"
+        },
+        {
+            "title": "The Witcher 3: Wild Hunt",
+            "description": "The Witcher 3: Wild Hunt is a story-driven, open-world RPG set in a rich and vibrant fantasy universe. As Geralt of Rivia, a professional monster hunter, players are thrust into a world of dense forests, bustling cities, and forgotten dungeons. The game is famed for its complex narrative, branching storylines, and deep character development, offering an immersive experience where each choice can lead to far-reaching consequences.",
+            "image": "muqsfnnlle6la2pdgp18",
+            "price": 39.99,
+            "categoryId": 1,
+            "categoryName": "RPG"
+        },
+        {
+            "title": "Stardew Valley",
+            "description": "Stardew Valley is a charming farming simulation game where players inherit their grandfather's old farm plot in Stardew Valley. The game combines farming, animal husbandry, crafting, and relationship building. Players can turn their overgrown fields into a thriving home, explore caves, encounter various creatures, and work to restore the community center to its former glory.",
+            "image": "fbtowkvbqmi8qo94r3k1",
+            "price": 14.99,
+            "categoryId": 3,
+            "categoryName": "Simulation"
+        },
+        {
+            "title": "Hades",
+            "description": "Hades is a rogue-like dungeon crawler where players defy the god of the dead as they hack and slash their way out of the Greek mythological Underworld. The game combines dynamic combat with a compelling story and beautiful art design. Each escape attempt empowers the player with more tools and options, while revealing more of the complex family drama between Hades and his son, the protagonist.",
+            "image": "yv3rvtvxhhduzaokjrrj",
+            "price": 24.99,
+            "categoryId": 4,
+            "categoryName": "Rogue-like"
+        },
+        {
+            "title": "Half-Life: Alyx",
+            "description": "Half-Life: Alyx marks Valve's VR return to the renowned Half-Life series. Set between the events of Half-Life and Half-Life 2, the game is a gripping story of survival, immersion, and pathos in a war-torn world invaded by aliens. Players take on the role of Alyx Vance in a fight against the Combine, using VR to manipulate tools and weapons, hack alien interfaces, and solve puzzles.",
+            "image": "syr9wgpffixbwiinnr0c",
+            "price": 59.99,
+            "categoryId": 5,
+            "categoryName": "VR-Shooter"
+        },
+        {
+            "title": "Divinity: Original Sin 2",
+            "description": "Divinity: Original Sin 2 is a critically acclaimed single-player and cooperative multiplayer fantasy RPG. The game features deep storytelling, rich world-building, and tactical combat. Players can choose their origins, explore the game's vast world, and make decisions that shape the narrative. The game's unique combat system allows for experimenting with different strategies, making each encounter a dynamic experience.",
+            "image": "elel2wgp5687jqxwsnxo",
+            "price": 44.99,
+            "categoryId": 1,
+            "categoryName": "RPG"
+        },
+        {
+            "title": "Among Us",
+            "description": "Among Us is an online and local party game of teamwork and betrayal for 4-10 players set in a space-themed environment. Players take on the roles of crewmates and impostors on a spaceship. Crewmates must complete tasks and try to identify the impostors, while impostors aim to covertly sabotage the mission. The game is a social deduction experience where communication and deception are key to survival and victory.",
+            "image": "xgide9yxemixpjpyjhms",
+            "price": 5.99,
+            "categoryId": 6,
+            "categoryName": "Party"
+        },
+        {
+            "title": "Doom Eternal",
+            "description": "Doom Eternal is the latest installment in the iconic FPS series, offering the ultimate combination of speed and power in push-forward, first-person combat. Players battle the forces of Hell across dimensions, using a wide array of powerful weapons. The game is known for its intense action, intricate level design, and heavy metal soundtrack, providing a thrilling and challenging gaming experience.",
+            "image": "vckcujitdjqh7i7ajkz3",
+            "price": 59.99,
+            "categoryId": 7,
+            "categoryName": "FPS"
+        },
+        {
+            "title": "Control",
+            "description": "Control is a visually stunning third-person action-adventure game that combines Remedy's trademark gunplay with supernatural abilities. Set in the shifting architecture of the Oldest House, players take on the role of Jesse Faden, the new Director of the Federal Bureau of Control, to uncover the truth about her past and the mysterious phenomena affecting reality. The game blends open-ended environments with an intense, narrative-driven experience.",
+            "image": "jjq7vlarfzzu3lgnz0ea",
+            "price": 29.99,
+            "categoryId": 8,
+            "categoryName": "Adventure"
+        },
+        {
+            "title": "Subnautica",
+            "description": "Subnautica is an underwater adventure game set on an alien ocean planet, where a massive, open world full of wonder and peril awaits. The game combines exploration, survival, and crafting as players dive into the depths of an alien underwater world filled with mysterious biomes, dangerous creatures, and rare resources. Players must manage their oxygen supply, craft equipment, and unravel the planet's enigmatic history.",
+            "image": "xmhrlip3hz53js34zk5u",
+            "price": 24.99,
+            "categoryId": 2,
+            "categoryName": "Survival"
+        }
+    ],
+    categories: [
+        {
+            "title": "rpg",
+        },
+        {
+            "title": "survival",
+        },
+        {
+            "title": "adventure",
+        },
+        {
+            "title": "fps",
+        },
+        {
+            "title": "party",
+        },
+        {
+            "title": "vr-shooter",
+        },
+        {
+            "title": "rogue-like",
+        },
+        {
+            "title": "simulation",
+        },
+    ]
+}
