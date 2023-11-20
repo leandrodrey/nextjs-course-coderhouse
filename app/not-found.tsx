d5c62eea21abb607bef851e7c9933460f61d16cd
Leandro Rey <leandrodrey@gmail.com>
2023-11-20 18:13:29 -0300

    Changed routes

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,7 @@
 import {NextPage} from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
-import CloudinaryImage from "@/app/services/CloudinaryImage";
+import CloudinaryImage from "@/services/CloudinaryImage";
 
 const NotFoundPage: NextPage = () => {
 
