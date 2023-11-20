a4dfa355201bb316bc585879f4b7c3c85ccc1818
Leandro Rey <leandrodrey@gmail.com>
2023-11-20 18:12:04 -0300

    Category Model

---
+++ models/Category.ts
@@ -0,0 +1,15 @@
+import mongoose, { Document, Model, Schema } from 'mongoose';
+import {ICategory} from "@/interfaces/ICategory";
+
+
+const categorySchema = new Schema<ICategory>({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    }
+}, { timestamps: true });
+
+const CategoryModel: Model<ICategory> = mongoose.models.Category || mongoose.model<ICategory>('Category', categorySchema);
+
+export default CategoryModel;
