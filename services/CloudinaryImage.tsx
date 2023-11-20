38c3d45f6d3eb8f278eb08b6c8fd428d78a3beb0
Leandro Rey <leandrodrey@gmail.com>
2023-11-20 18:12:16 -0300

    Use env

--- services/CloudinaryImage.tsx
+++ services/CloudinaryImage.tsx
@@ -4,17 +4,15 @@ const CloudinaryImage = (imagePath: string): string => {
 
     const myCloudinary = new Cloudinary({
         cloud: {
-            cloudName: "dmef6dgiq",
+            cloudName: process.env.CLOUDINARY_CLOUD_NAME,
         },
     });
-
     const cloudinaryImg = myCloudinary.image(`gamebazar/${imagePath}`);
     const cloudinaryImageUrl = cloudinaryImg.toURL();
 
     return (
         cloudinaryImageUrl
     )
-
 }
 
 export default CloudinaryImage;
