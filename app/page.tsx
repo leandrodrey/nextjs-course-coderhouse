c9e99179c78e7d1160fbc7dac991b354a0008564
Leandro Rey <leandrodrey@gmail.com>
2023-11-22 18:12:40 -0300

    Fetch added to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
 import {ReactElement} from "react";
 import ProductsContainer from "@/app/components/ui/ProductsContainer";
-import data from "@/app/items.json";
 
-export default function Home(): ReactElement {
+async function getProductByCategory(category: number | string) {
+    const res = await fetch(`http://localhost:3000/api/products/category/${category}`);
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
+    return res.json();
+}
+
+export default async function Home(): Promise<ReactElement> {
+
+    const allProducts = await getProductByCategory('all');
+
     return (
         <>
-            <ProductsContainer products={data}/>
+            <ProductsContainer products={allProducts}/>
         </>
     )
 }
