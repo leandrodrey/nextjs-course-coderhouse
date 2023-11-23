49165fb95e0d3fa16cb64e18064aa5e63ea605af
Leandro Rey <leandrodrey@gmail.com>
2023-11-22 21:11:33 -0300

    Created categories api and service

---
+++ app/api/categories/route.tsx
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from 'next/server';
+import CategoryModel from '@/models/Category';
+import { db } from "@/database";
+
+export async function GET(request: NextRequest): Promise<NextResponse> {
+    await db.connect();
+    try {
+        const categories = await CategoryModel.find();
+        return new NextResponse(JSON.stringify(categories), {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    } catch (error) {
+        let errorMessage = 'An unknown error occurred';
+        if (error instanceof Error) {
+            errorMessage = error.message;
+        }
+        return new NextResponse(JSON.stringify({ error: errorMessage }), {
+            status: 500,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    } finally {
+        await db.disconnect();
+    }
+}
---
+++ services/CategoryService.ts
@@ -0,0 +1,9 @@
+import {ICategory} from "@/interfaces/ICategory";
+
+export async function getAllCategories(): Promise<ICategory[]> {
+    const res = await fetch(`${process.env.API_CATEGORIES_URL}`);
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
+    return res.json();
+}
