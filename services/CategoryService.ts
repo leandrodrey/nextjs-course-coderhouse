9c2afc5b6d6cc646d8195ae939c6c91eccd667d6
Leandro Rey <leandrodrey@gmail.com>
2023-11-24 16:11:49 -0300

    Config updated

--- services/CategoryService.ts
+++ services/CategoryService.ts
@@ -1,7 +1,7 @@
 import {ICategory} from "@/interfaces/ICategory";
 
 export async function getAllCategories(): Promise<ICategory[]> {
-    const res = await fetch(`${process.env.API_CATEGORIES_URL}`);
+    const res = await fetch(`${process.env.API_CATEGORIES_URL}, { cache: 'no-store' }`);
     if (!res.ok) {
         throw new Error('Failed to fetch data')
     }
--- services/ProductService.ts
+++ services/ProductService.ts
@@ -1,7 +1,7 @@
 import { IProduct } from "@/interfaces/IProduct";
 
 export async function getProductById(productId: number): Promise<IProduct> {
-    const res = await fetch(`${process.env.API_PRODUCTS_URL}/${productId}`);
+    const res = await fetch(`${process.env.API_PRODUCTS_URL}/${productId}`, { cache: 'no-store' });
     if (!res.ok) {
         throw new Error('Failed to fetch data')
     }
@@ -9,7 +9,7 @@ export async function getProductById(productId: number): Promise<IProduct> {
 }
 
 export async function getProductByCategory(category: number | string): Promise<IProduct[]> {
-    const res = await fetch(`${process.env.API_PRODUCTS_URL}/category/${category}`);
+    const res = await fetch(`${process.env.API_PRODUCTS_URL}/category/${category}`, { cache: 'no-store' });
     if (!res.ok) {
         throw new Error('Failed to fetch data')
     }
