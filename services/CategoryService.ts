ea4cc44a1f3d69337ac5f33cb2f94ee420b05967
Leandro Rey <leandrodrey@gmail.com>
2023-12-01 14:49:24 -0300

    Removed cache option from API fetch request
    The cache option in the fetch request to API_CATEGORIES_URL was removed. This was done to facilitate a seamless connection with MongoDB as the 'no-store' cache was creating issues with data retrieval. The submitted fetch request now strictly refers to the API URL environment variable.

--- services/CategoryService.ts
+++ services/CategoryService.ts
@@ -1,9 +1,10 @@
 import {ICategory} from "@/interfaces/ICategory";
 
 export async function getAllCategories(): Promise<ICategory[]> {
-    const res = await fetch(`${process.env.API_CATEGORIES_URL}, { cache: 'no-store' }`);
+    const res = await fetch(`${process.env.API_CATEGORIES_URL}`);
     if (!res.ok) {
-        throw new Error('Failed to fetch data')
+        throw new Error('Failed to fetch data');
     }
     return res.json();
 }
+
