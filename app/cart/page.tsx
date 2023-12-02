0465278b9de797b617724c6b705b2644d82cedfa
Leandro Rey <leandrodrey@gmail.com>
2023-12-01 23:56:12 -0300

    Refactor Cart component to remove CartContext dependency
    The Cart component is updated by removing useContext from imports and CartContext usage from the component. Instead, of passing cart items as props to CartTable, CartTable is now responsible for fetching the Cart context. This shifts the responsibility of context management to CartTable and makes the Cart component simpler and easy to test.

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,17 +1,14 @@
 "use client"
 import {ReactElement, useContext} from "react";
-import {CartContext} from "@/context/CartProvider";
 import CartTable from "@/app/components/ui/CartTable";
 
 export default function Cart(): ReactElement {
-
-    const {cart} = useContext(CartContext);
 
     return (
         <>
             <div className="container mx-auto mt-10 md:p-5">
                 <h1 className='mb-2'>Carrito de Compras</h1>
-                <CartTable cartItems={cart}/>
+                <CartTable />
             </div>
         </>
     )
