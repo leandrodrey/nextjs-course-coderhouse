db76f8fd8975595171e3f812718d86a8292c47f2
Leandro Rey <leandrodrey@gmail.com>
2023-12-01 22:46:51 -0300

    Add useItemCount hook for managing item counts.
    A new useItemCount hook has been introduced. This custom hook manages the individual count of items. It has two main functions, handleSum and handleRest, which respectively increment and decrement the count state. This addition provides a modular way to manage item counts, facilitating reusability and cleaner code.

---
+++ hooks/useItemCount.tsx
@@ -0,0 +1,17 @@
+"use client"
+import {useState} from 'react'
+
+const UseItemCount = () => {
+
+    const [count, setCount] = useState(1);
+    const handleSum = () => setCount(count + 1);
+    const handleRest = () => setCount(count - 1);
+
+    return {
+        count,
+        handleSum,
+        handleRest
+    }
+}
+
+export default UseItemCount
