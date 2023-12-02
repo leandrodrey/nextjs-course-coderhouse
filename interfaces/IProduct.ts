85087f3e79f9f4c8906cba2e3d5b94b18a2ca2ca
Leandro Rey <leandrodrey@gmail.com>
2023-12-02 00:08:33 -0300

    Make product's id required in IProduct interface
    The optional status of the `id` field in the IProduct interface was removed, making `id` mandatory. This will enforce a uniform data structure throughout the application and prevent any potential bugs related to missing ids for products.

--- interfaces/IProduct.ts
+++ interfaces/IProduct.ts
@@ -2,7 +2,7 @@ import { Types } from 'mongoose';
 
 export interface IProduct {
     _id: Types.ObjectId;
-    id?: string;
+    id: string;
     title: string;
     description: string;
     image: string;
