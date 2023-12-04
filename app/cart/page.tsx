24e717aadae26ff2110c04365e0be55489154858
Leandro Rey <leandrodrey@gmail.com>
2023-12-04 20:37:39 -0300

    "Refactor Cart page to use ProductsTableContainer"
    This commit removes the use of CartTable in the Cart page and replaces it with the more versatile ProductsTableContainer. The ProductsTableContainer is configured to act as a CartTable, providing consistency in table rendering across the application. This change also includes an update to the heading style for visual enhancement.

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,6 +1,5 @@
-"use client"
 import {ReactElement, useContext} from "react";
-import CartTable from "@/app/components/ui/CartTable";
+import ProductsTableContainer from "@/app/components/ui/ProductsTableContainer";
 
 export const dynamic = 'force-dynamic';
 
@@ -9,8 +8,8 @@ export default function Cart(): ReactElement {
     return (
         <>
             <div className="container mx-auto mt-10 md:p-5">
-                <h1 className='mb-2'>Carrito de Compras</h1>
-                <CartTable />
+                <h1 className='mb-2 text-3xl'>Carrito de Compras</h1>
+                <ProductsTableContainer action='remove' context='cart' />
             </div>
         </>
     )
