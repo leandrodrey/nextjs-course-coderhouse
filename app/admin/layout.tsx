bbfc2ea08431dd937d195d709f3de351b0728c1f
Leandro Rey <leandrodrey@gmail.com>
2023-12-04 20:36:38 -0300

    Changed background color in the admin layout component

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -4,7 +4,7 @@ export default function DashboardLayout({ children,}: {
     children: ReactNode
 }) {
     return (
-        <div className="flex h-full bg-gray-100">
+        <div className="flex h-full">
             {children}
         </div>
     )
