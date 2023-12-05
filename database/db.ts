6f3ff4183f45a6f663b0051c0804988f23aa49c7
Leandro Rey <leandrodrey@gmail.com>
2023-12-05 16:34:09 -0300

    Refactor MongoDB connection management
    This commit simplifies and refactors the way MongoDB connection management is handled in `db.ts`. Earlier, unnecessarily verbose logs were printed, and an unnecessary try catch was surrounding the connection. This has been removed to simplify the connection process. Also, a log is added when the connection is ended, and if the environment is in development, the connection is not ended to assist with debugging. New log messages are modified to be more informative.

--- database/db.ts
+++ database/db.ts
@@ -13,36 +13,33 @@ const mongoConnection = {
 
 export const connect = async () => {
     if (mongoConnection.isConnected) {
-        console.log('Already connected to MongoDB');
+        console.log('Using existing connection');
         return;
     }
 
     if (mongoose.connections.length > 0) {
         mongoConnection.isConnected = mongoose.connections[0].readyState;
         if (mongoConnection.isConnected === 1) {
-            console.log('Using existing database connection');
+            console.log('Using anterior connection');
             return;
         }
 
-        console.log('Disconnecting existing database connection');
         await mongoose.disconnect();
     }
 
-    try {
-        await mongoose.connect(process.env.MONGO_URL || '', {});
-        mongoConnection.isConnected = 1;
-        console.log('Connected to MongoDB');
-    } catch (error) {
-        console.error('Error connecting to MongoDB', error);
-        mongoConnection.isConnected = 0;
-        throw error;
-    }
+    await mongoose.connect(process.env.MONGO_URL || '');
+    mongoConnection.isConnected = 1;
+    console.log('Connected to MongoDB', process.env.MONGO_URL || '');
 }
 
 export const disconnect = async () => {
+
+    if (process.env.NODE_ENV === 'development') return;
 
     if (mongoConnection.isConnected === 0) return;
 
     await mongoose.disconnect();
     mongoConnection.isConnected = 0;
+
+    console.log('Disconnected from MongoDB');
 }
