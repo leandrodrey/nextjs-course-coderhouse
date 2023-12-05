7c713f9b1a73aafca2e90f6155dd8c3ced179874
Leandro Rey <leandrodrey@gmail.com>
2023-12-04 22:35:30 -0300

    Updated the variable name in CloudinaryImage.tsx to match the change in the .env.example file.
    The cloud name configuration in the Cloudinary setup has been changed from 'CLOUDINARY_CLOUD_NAME' to 'NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME'. This change is to ensure consistency in environment variable naming across the project. Users who follow the .env.example file will be able to successfully configure their cloud name without encountering errors.

--- services/CloudinaryImage.tsx
+++ services/CloudinaryImage.tsx
@@ -4,7 +4,7 @@ const CloudinaryImage = (imagePath: string): string => {
 
     const myCloudinary = new Cloudinary({
         cloud: {
-            cloudName: process.env.CLOUDINARY_CLOUD_NAME
+            cloudName: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME
         },
     });
     const cloudinaryImg = myCloudinary.image(`gamebazar/${imagePath}`);
