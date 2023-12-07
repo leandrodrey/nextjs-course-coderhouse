a730ee4956b1c35b4ec17ca3c23ce65b9f756ef0
Leandro Rey <leandrodrey@gmail.com>
2023-12-07 19:20:26 -0300

    Relocate session handling logic to layout file
    The session handling code which was previously in 'page.tsx' has been removed and implemented in 'layout.tsx'. This streamlines the logic of handling user sessions and also allows for more flexible rendering based on session status. The code now includes a redirect for non-authenticated users and also displays session-specific information such as the user's email if a session exists.

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,11 +1,31 @@
+"use client"
 import {ReactNode} from "react";
+import {signIn, signOut, useSession} from "next-auth/react";
+import {redirect} from "next/navigation";
 
 export default function DashboardLayout({ children,}: {
     children: ReactNode
 }) {
+
+    const {data: session, status} = useSession();
+
+    if (!session) redirect('api/auth/signin')
+
+    if (session) {
+        return (
+            <div>
+                <p>Bienvenido, {session.user?.email}</p>
+                <button onClick={() => signOut()}>Cerrar Sesión</button>
+                <div className="flex flex-col h-full flex-1">
+                    {children}
+                </div>
+            </div>
+        );
+    }
     return (
-        <div className="flex h-full flex-1 flex flex-col">
-            {children}
+        <div>
+            <button onClick={() => signIn()}>Iniciar Sesión</button>
         </div>
-    )
+    );
+
 }
