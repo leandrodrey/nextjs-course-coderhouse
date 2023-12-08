72524c88a4e3fa3c0acd927b641768ebed93743d
Leandro Rey <leandrodrey@gmail.com>
2023-12-08 19:53:46 -0300

    Refactor product detail page and extract detail card as separate component
    The product detail page component is refactored for better reusability and maintainability. All code relevant to the display of the product in detail is extracted to a new separate component called 'ProductCardDetail'. The name 'ProductDetail' component is changed to 'ProductDetailPage' to better reflect its function.

--- app/detail/[id]/page.tsx
+++ app/detail/[id]/page.tsx
@@ -1,11 +1,8 @@
 import {FC} from "react";
 import type {Metadata, ResolvingMetadata} from 'next';
-import Image from 'next/image';
-import Link from "next/link";
 import {IProductWithCategory} from "@/interfaces/IProduct";
-import CloudinaryImage from "@/services/CloudinaryImage";
 import {getProductById} from "@/services/ProductService";
-import AddToCart from "@/components/ui/AddToCart";
+import ProductCardDetail from "@/components/ProductCardDetail";
 
 export const dynamic = 'force-dynamic';
 
@@ -36,7 +33,7 @@ interface ProductDetailProps {
     };
 }
 
-const ProductDetail: FC<ProductDetailProps> = async ({params}) => {
+const ProductDetailPage: FC<ProductDetailProps> = async ({params}) => {
 
     const {id} = params;
     const product: IProductWithCategory = await getProductById(id);
@@ -45,42 +42,11 @@ const ProductDetail: FC<ProductDetailProps> = async ({params}) => {
         return <div className="text-center text-xl text-red-500 p-5">Producto no encontrado</div>;
     }
 
-    const productImage = CloudinaryImage(product.image);
-
     return (
         <div className="container mx-auto mt-10 md:p-5">
-            <div className="md:flex md:items-stretch">
-                <div className="w-full md:w-1/2 lg:w-1/3 flex justify-center mb-4 md:mb-0">
-                    <Image
-                        src={productImage}
-                        alt={`Imagen de ${product.title}`}
-                        width={640}
-                        height={640}
-                        className="object-contain"
-                        placeholder = "blur"
-                        blurDataURL={productImage}
-                    />
-                </div>
-                <div className="w-full md:w-1/2 lg:w-2/3 bg-stone-900 bg-opacity-90 rounded-lg p-6 shadow-xl flex flex-col justify-between">
-                    <div className="">
-                        <div className="flex justify-between items-center mb-5">
-                            <h1 className="text-3xl font-bold text-blue-300 mb-3">{product.title}</h1>
-                            <span className="text-3xl text-gray-100">${product.price}</span>
-                        </div>
-                        <p className="text-gray-300 text-sm mb-4">{product.description}</p>
-                        <div className="flex justify-between items-center">
-                            <Link href={`/category/${product.categoryName}`}>
-                                <span className="capitalize px-2 py-1 bg-blue-200 text-blue-900 text-sm font-semibold rounded-full transition duration-300 hover:bg-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-opacity-50">
-                                    {product.categoryName}
-                                </span>
-                            </Link>
-                            <AddToCart product={product}/>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <ProductCardDetail product={product} />
         </div>
     );
 };
 
-export default ProductDetail;
+export default ProductDetailPage;
---
+++ components/ProductCardDetail.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import Link from "next/link";
+import AddToCart from "@/components/ui/AddToCart";
+import CloudinaryImage from "@/services/CloudinaryImage";
+import {FC} from "react";
+import {IProductWithCategory} from "@/interfaces/IProduct";
+
+interface ProductCardDetailProps {
+    product: IProductWithCategory
+}
+
+const ProductCardDetail: FC<ProductCardDetailProps> = ({product}) => {
+
+    const productImage = CloudinaryImage(product.image);
+
+    return (
+        <div className="md:flex md:items-stretch">
+            <div className="w-full md:w-1/2 lg:w-1/3 flex justify-center mb-4 md:mb-0">
+                <Image
+                    src={productImage}
+                    alt={`Imagen de ${product.title}`}
+                    width={640}
+                    height={640}
+                    className="object-contain"
+                    placeholder="blur"
+                    blurDataURL="/loading.png"
+                />
+            </div>
+            <div className="w-full md:w-1/2 lg:w-2/3 bg-stone-900 bg-opacity-90 rounded-lg p-6 shadow-xl flex flex-col justify-between">
+                <div className="">
+                    <div className="flex justify-between items-center mb-5">
+                        <h1 className="text-3xl font-bold text-blue-300 mb-3">{product.title}</h1>
+                        <span className="text-3xl text-gray-100">${product.price}</span>
+                    </div>
+                    <p className="text-gray-300 text-sm mb-4">{product.description}</p>
+                    <div className="flex justify-between items-center">
+                        <Link href={`/category/${product.categoryName}`}>
+                                <span className="capitalize px-2 py-1 bg-blue-200 text-blue-900 text-sm font-semibold rounded-full transition duration-300 hover:bg-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-opacity-50">
+                                    {product.categoryName}
+                                </span>
+                        </Link>
+                        <AddToCart product={product}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProductCardDetail;
