c35db90cbbb41ee93fcbc73698a8656d467669c4
Leandro Rey <leandrodrey@gmail.com>
2023-12-09 04:31:18 -0300

    Change anchor tags to Link tags in SocialIcons component
    Updated `SocialIcons.tsx` by substituting "a" tags with "Link" from Next.js for better performance and routing. This alteration ultimately improves user experience on page navigation and boosts overall site speed.

--- components/ui/SocialIcons.tsx
+++ components/ui/SocialIcons.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import Link from "next/link";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import InstagramIcon from "@mui/icons-material/Instagram";
@@ -10,15 +11,15 @@ interface SocialIconsProps {
 const SocialIcons: FC<SocialIconsProps> = ({ menuOpen }) => {
     return (
         <div className={`flex ${menuOpen ? "space-x-3 opacity-100" : "space-y-3 opacity-50 flex-col items-center"} justify-center transition-opacity duration-500 `}>
-            <a href="#" className="text-xs text-blue-300 hover:text-white transition duration-300">
+            <Link href="#" className="text-xs text-blue-300 hover:text-white transition duration-300">
                 <FacebookIcon fontSize={menuOpen ? "large" : "medium"} />
-            </a>
-            <a href="#" className="text-blue-300 hover:text-white transition duration-300">
+            </Link>
+            <Link href="#" className="text-blue-300 hover:text-white transition duration-300">
                 <TwitterIcon fontSize={menuOpen ? "large" : "medium"} />
-            </a>
-            <a href="#" className="text-blue-300 hover:text-white transition duration-300">
+            </Link>
+            <Link href="#" className="text-blue-300 hover:text-white transition duration-300">
                 <InstagramIcon fontSize={menuOpen ? "large" : "medium"} />
-            </a>
+            </Link>
         </div>
     )
 }
