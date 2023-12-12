0159c97a7ac1297f345930502ae06d6ec27dbdb6
Leandro Rey <leandrodrey@gmail.com>
2023-12-11 23:30:44 -0300

    Improve CartWidget Link styling

--- components/ui/CartWidget.tsx
+++ components/ui/CartWidget.tsx
@@ -14,7 +14,7 @@ const CartWidget: FC<CartWidgetProps> = ({ open }) => {
 
     return (
         <div className='flex rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 relative mt-9'>
-            <Link href='/cart' className='flex'>
+            <Link href='/cart' className='flex items-center'>
                 <ShoppingCartIcon className="h-6 w-6" />
                 <span className={`${!open && "hidden"} origin-left duration-200 hover:text-blue-300 capitalize ml-4`}>
                     Shopping Cart
