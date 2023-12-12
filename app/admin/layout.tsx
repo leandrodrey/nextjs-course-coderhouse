c71a89c723c72531f0a1f6ef7d71544eae97507f
Leandro Rey <leandrodrey@gmail.com>
2023-12-11 22:28:11 -0300

    Refactor code and modify icon imports in ProductsTable Files

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,31 +1,26 @@
 "use client"
 import {ReactNode} from "react";
-import {signIn, signOut, useSession} from "next-auth/react";
 import {redirect} from "next/navigation";
+import {signOut, useSession} from "next-auth/react";
 
-export default function DashboardLayout({ children,}: {
+export default function DashboardLayout({ children }: {
     children: ReactNode
 }) {
 
     const {data: session, status} = useSession();
 
-    if (!session) redirect('api/auth/signin')
+    if (!session) redirect('api/auth/signin');
 
     if (session) {
         return (
             <div>
                 <p>Bienvenido, {session.user?.email}</p>
-                <button onClick={() => signOut()}>Cerrar Sesión</button>
+                <button onClick={() => signOut()}>Logout</button>
                 <div className="flex flex-col h-full flex-1">
                     {children}
                 </div>
             </div>
         );
     }
-    return (
-        <div>
-            <button onClick={() => signIn()}>Iniciar Sesión</button>
-        </div>
-    );
 
 }
