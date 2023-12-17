cce7861aa302bba98fbdc3c9521cdccd0eccaba9
Leandro Rey <leandrodrey@gmail.com>
2023-12-17 12:33:22 -0300

    Merge OrderFormService code into OrderForm component

--- components/forms/orderForm.tsx
+++ components/forms/orderForm.tsx
@@ -1,7 +1,8 @@
 'use client'
-import React from 'react';
+import React, {FC, useContext} from 'react';
 import {ErrorMessage, Field, Form, Formik} from 'formik';
 import * as Yup from 'yup';
+import {CartContext} from "@/context/CartProvider";
 
 const initialValues = {
     fullName: '',
@@ -21,59 +22,94 @@ const OrderSchema = Yup.object().shape({
     country: Yup.string().required('Required'),
 });
 
-const OrderForm = () => (
-    <Formik
-        initialValues={initialValues}
-        validationSchema={OrderSchema}
-        onSubmit={(values, {setSubmitting}) => {
-            setSubmitting(false);
-        }}
-    >
-        {({isSubmitting}) => (
-            <Form className="p-6">
+const OrderForm: FC = () => {
 
-                <div className="mb-4">
-                    <label htmlFor="fullName" className="block text-white text-sm font-bold mb-2">Full Name</label>
-                    <Field type="text" name="fullName" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
-                    <ErrorMessage name="fullName" component="div" className="text-red-300 text-xs italic"/>
-                </div>
+    const { cart } = useContext(CartContext);
 
-                <div className="mb-4">
-                    <label htmlFor="email" className="block text-white text-sm font-bold mb-2">Email</label>
-                    <Field name="email" type="email" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
-                    <ErrorMessage name="email" component="div" className="text-red-500 text-xs italic"/>
-                </div>
+    const handleSubmit = async (values, { setSubmitting }) => {
 
-                <div className="mb-4">
-                    <label htmlFor="phone" className="block text-white text-sm font-bold mb-2">Phone</label>
-                    <Field name="phone" type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
-                    <ErrorMessage name="phone" component="div" className="text-red-500 text-xs italic"/>
-                </div>
+        const orderData = {
+            ...values,
+            cartItems: cart.items,
+            totalPayment: cart.totalPayment,
+        };
 
-                <div className="mb-4 ">
-                    <label htmlFor="address" className="block text-white text-sm font-bold mb-2">Address</label>
-                    <Field name="address" type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
-                    <ErrorMessage name="address" component="div" className="text-red-500 text-xs italic"/>
-                </div>
+        try {
+            const response = await fetch('/api/orders', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(orderData),
+            });
 
-                <div className="mb-4">
-                    <label htmlFor="city" className="block text-white text-sm font-bold mb-2">City</label>
-                    <Field name="city" type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
-                    <ErrorMessage name="city" component="div" className="text-red-500 text-xs italic"/>
-                </div>
+            if (!response.ok) {
+                throw new Error(`Error: ${response.status}`);
+            }
 
-                <div className="mb-4">
-                    <label htmlFor="country" className="block text-white text-sm font-bold mb-2">Country</label>
-                    <Field name="country" type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
-                    <ErrorMessage name="country" component="div" className="text-red-500 text-xs italic"/>
-                </div>
+            const data = await response.json();
+            console.log(data.message);
 
-                <button type="submit" disabled={isSubmitting} className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                    Submit Order
-                </button>
-            </Form>
-        )}
-    </Formik>
-);
+            // Aquí podrías redirigir o manejar la respuesta exitosa.
+        } catch (error) {
+            console.error('Error submitting order:', error);
+        }
+
+        setSubmitting(false);
+    };
+
+    return (
+        <Formik
+            initialValues={initialValues}
+            validationSchema={OrderSchema}
+            onSubmit={handleSubmit}
+        >
+            {({isSubmitting}) => (
+                <Form className="p-6">
+
+                    <div className="mb-4">
+                        <label htmlFor="fullName" className="block text-white text-sm font-bold mb-2">Full Name</label>
+                        <Field type="text" name="fullName" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
+                        <ErrorMessage name="fullName" component="div" className="text-red-300 text-xs italic"/>
+                    </div>
+
+                    <div className="mb-4">
+                        <label htmlFor="email" className="block text-white text-sm font-bold mb-2">Email</label>
+                        <Field name="email" type="email" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
+                        <ErrorMessage name="email" component="div" className="text-red-500 text-xs italic"/>
+                    </div>
+
+                    <div className="mb-4">
+                        <label htmlFor="phone" className="block text-white text-sm font-bold mb-2">Phone</label>
+                        <Field name="phone" type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
+                        <ErrorMessage name="phone" component="div" className="text-red-500 text-xs italic"/>
+                    </div>
+
+                    <div className="mb-4 ">
+                        <label htmlFor="address" className="block text-white text-sm font-bold mb-2">Address</label>
+                        <Field name="address" type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
+                        <ErrorMessage name="address" component="div" className="text-red-500 text-xs italic"/>
+                    </div>
+
+                    <div className="mb-4">
+                        <label htmlFor="city" className="block text-white text-sm font-bold mb-2">City</label>
+                        <Field name="city" type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
+                        <ErrorMessage name="city" component="div" className="text-red-500 text-xs italic"/>
+                    </div>
+
+                    <div className="mb-4">
+                        <label htmlFor="country" className="block text-white text-sm font-bold mb-2">Country</label>
+                        <Field name="country" type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
+                        <ErrorMessage name="country" component="div" className="text-red-500 text-xs italic"/>
+                    </div>
+
+                    <button type="submit" disabled={isSubmitting} className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                        Submit Order
+                    </button>
+                </Form>
+            )}
+        </Formik>
+    );
+};
 
 export default OrderForm;
