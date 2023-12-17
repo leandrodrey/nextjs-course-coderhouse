1bd3b4017d18c7ea47ef4ecb15eb109d01eb9959
leandrodrey <leandrodrey@gmail.com>
2023-12-17 17:25:39 -0300

    Improve 'ProductsTableContainer' user interface

--- components/ui/ProductsTableContainer.tsx
+++ components/ui/ProductsTableContainer.tsx
@@ -7,8 +7,10 @@ import ProductsTable from "@/components/ui/ProductsTable";
 import ProductsTableCard from "@/components/ui/ProductsTableCard";
 import EmptyCart from "@/components/ui/EmptyCart";
 import Loader from "@/components/ui/Loader";
+import ButtonWithLink from "@/components/ui/ButtonWithLink";
+import Link from "next/link";
 
-const ProductsTableContainer:FC<IProductTable> = ({action= 'remove', context= 'cart', products}) => {
+const ProductsTableContainer: FC<IProductTable> = ({action = 'remove', context = 'cart', products}) => {
 
     const {cart} = useContext(CartContext);
     const [isLoading, setIsLoading] = useState(true);
@@ -23,22 +25,32 @@ const ProductsTableContainer:FC<IProductTable> = ({action= 'remove', context= 'c
     }, [cart.items, products, context]);
 
     if (isLoading) {
-        return <Loader />;
+        return <Loader/>;
     }
 
     if (!componentProducts.length) {
         if (context === 'cart') {
-            return <EmptyCart />;
+            return <EmptyCart/>;
         } else if (context === 'admin') {
-            return ("TODO - Add a message for empty products")
+            return (
+                <>
+                    <p>There is no product yet.</p>
+                    <Link className="text-blue-500 hover:text-blue-700" href="/admin/products/create">Add a product</Link>
+                </>
+            )
         }
     }
 
     return (
         <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
-            <ProductsTable products={componentProducts} action={action} context={context} />
-            <ProductsTableCard products={componentProducts} action={action} context={context} />
-            <p className="text-lg text-gray-300 mt-4 text-right">Total Order: <span className='text-2xl'>${cart.totalPayment.toFixed(2)}</span></p>
+            <ProductsTable products={componentProducts} action={action} context={context}/>
+            <ProductsTableCard products={componentProducts} action={action} context={context}/>
+            {context === 'cart' && (
+                <>
+                    <p className="text-lg text-gray-300 mt-4 text-right">Total Order: <span className='text-2xl'>${cart.totalPayment.toFixed(2)}</span></p>
+                    <ButtonWithLink url="/cart/checkout" text="Proceed to Checkout"/>
+                </>
+            )}
         </div>
     );
 }
