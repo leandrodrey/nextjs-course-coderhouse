238ef03acdd6c62a2817e19ebdf562934158c946
leandrodrey <leandrodrey@gmail.com>
2023-12-17 17:24:58 -0300

    Implement stock update after order in API

--- app/api/orders/route.tsx
+++ app/api/orders/route.tsx
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import OrderModel from '@/models/Order';
 import { db } from '@/database';
 import mongoose from "mongoose";
+import ProductModel from "@/models/Products";
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
     await db.connect();
@@ -21,6 +22,18 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
                 headers: { 'Content-Type': 'application/json' }
             });
         }
+
+        const updateStockPromises = orderData.products.map(async (product: { productId: string; quantity: number; }) => {
+            const dbProduct = await ProductModel.findById(product.productId);
+            if (dbProduct) {
+                dbProduct.stock -= product.quantity;
+                return dbProduct.save();
+            } else {
+                throw new Error(`Product with id ${product.productId} not found`);
+            }
+        });
+
+        await Promise.all(updateStockPromises);
 
         const newOrder = new OrderModel(orderData);
         const savedOrder = await newOrder.save();
