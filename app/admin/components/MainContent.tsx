7d0ccac967a68180b00da8844af3004e69ba439d
leandrodrey <leandrodrey@gmail.com>
2023-12-17 17:23:34 -0300

    Modify 'Add Product' button into link on Main Content

--- app/admin/components/MainContent.tsx
+++ app/admin/components/MainContent.tsx
@@ -12,27 +12,17 @@ const MainContent: FC<MainContentProps> = ({products}) => {
 
     return (
         <>
-            <div className="flex md:hidden text-black justify-between p-2">
-                <Link href="/admin" className="">
-                    Productos
-                </Link>
-                <Link href="/admin" className="">
-                    Productos
-                </Link>
-                <Link href="/admin" className="">
-                    Productos
-                </Link>
-            </div>
-
             <div className="flex-1">
                 <div className="py-6">
                     <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
                         <div className="shadow rounded-lg p-6">
                             <div className="flex justify-between items-center mb-8">
                                 <h1 className="mb-5 text-3xl text-gray-200">Dashboard</h1>
-                                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                                    Add Product
-                                </button>
+                                <Link href="/admin/products/create">
+                                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                        Add Product
+                                    </button>
+                                </Link>
                             </div>
                             <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
                                 <ProductsTableContainer action="edit" context="admin" products={products} />
