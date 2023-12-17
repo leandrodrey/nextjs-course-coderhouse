530ed612cbd910a9d514ef9f8f68e884728ef776
Leandro Rey <leandrodrey@gmail.com>
2023-12-17 12:27:17 -0300

    Add Category API and OrderFormService; refactor order form

--- components/forms/orderForm.tsx
+++ components/forms/orderForm.tsx
@@ -31,49 +31,43 @@ const OrderForm = () => (
     >
         {({isSubmitting}) => (
             <Form className="p-6">
-                {/* Full Name */}
+
                 <div className="mb-4">
                     <label htmlFor="fullName" className="block text-white text-sm font-bold mb-2">Full Name</label>
                     <Field type="text" name="fullName" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
                     <ErrorMessage name="fullName" component="div" className="text-red-300 text-xs italic"/>
                 </div>
 
-                {/* Email */}
                 <div className="mb-4">
                     <label htmlFor="email" className="block text-white text-sm font-bold mb-2">Email</label>
                     <Field name="email" type="email" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
                     <ErrorMessage name="email" component="div" className="text-red-500 text-xs italic"/>
                 </div>
 
-                {/* Phone */}
                 <div className="mb-4">
                     <label htmlFor="phone" className="block text-white text-sm font-bold mb-2">Phone</label>
                     <Field name="phone" type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
                     <ErrorMessage name="phone" component="div" className="text-red-500 text-xs italic"/>
                 </div>
 
-                {/* Address */}
                 <div className="mb-4 ">
                     <label htmlFor="address" className="block text-white text-sm font-bold mb-2">Address</label>
                     <Field name="address" type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
                     <ErrorMessage name="address" component="div" className="text-red-500 text-xs italic"/>
                 </div>
 
-                {/* City */}
                 <div className="mb-4">
                     <label htmlFor="city" className="block text-white text-sm font-bold mb-2">City</label>
                     <Field name="city" type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
                     <ErrorMessage name="city" component="div" className="text-red-500 text-xs italic"/>
                 </div>
 
-                {/* Country */}
                 <div className="mb-4">
                     <label htmlFor="country" className="block text-white text-sm font-bold mb-2">Country</label>
                     <Field name="country" type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
                     <ErrorMessage name="country" component="div" className="text-red-500 text-xs italic"/>
                 </div>
 
-                {/* Submit Button */}
                 <button type="submit" disabled={isSubmitting} className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                     Submit Order
                 </button>
