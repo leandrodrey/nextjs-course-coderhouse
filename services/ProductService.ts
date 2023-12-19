053bf5e4d1a008559e2f13be28ae3e8b445f695b
Leandro Rey <leandrodrey@gmail.com>
2023-12-19 03:35:59 -0300

    Update ProductService to support delete operation
    This update modifies the `getProductById` method in ProductService to accept productId as string and also adds a new `deleteProductById` method. The new deletion method fetches specific product data with a DELETE http request and throws an error if the request is unsuccessful.

--- services/ProductService.ts
+++ services/ProductService.ts
@@ -1,6 +1,6 @@
 import { IProduct } from "@/interfaces/IProduct";
 
-export async function getProductById(productId: number): Promise<IProduct> {
+export async function getProductById(productId: string): Promise<IProduct> {
     const res = await fetch(`${process.env.API_PRODUCTS_URL}/${productId}`, { cache: 'no-store' });
     if (!res.ok) {
         throw new Error('Failed to fetch data')
@@ -14,4 +14,13 @@ export async function getProductByCategory(category: number | string): Promise<I
         throw new Error('Failed to fetch data')
     }
     return res.json();
+}
+
+export async function deleteProductById(productId: string): Promise<void> {
+    const res = await fetch(`api/products/${productId}`, { method: 'DELETE' });
+    if (!res.ok) {
+        const errorBody = await res.json();
+        const errorMessage = errorBody.error || 'Failed to delete the product';
+        throw new Error(errorMessage);
+    }
 }
