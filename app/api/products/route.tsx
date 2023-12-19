05f970f747f9513bc9f8e6dd5c6b75e415c8a0b7
Leandro Rey <leandrodrey@gmail.com>
2023-12-19 03:34:28 -0300

    Refactor product API to simplify data input
    This update simplifies the process of adding new products to the database through the product API. Now the API accepts direct JSON objects for product data, eliminating the need for multipart form data. Moreover, it validates the category ID using the mongoose's isValidObjectId function, ensuring proper data integrity.

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,70 +1,25 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { promises as fsPromises, createWriteStream } from 'fs';
-import path from 'path';
+import {NextRequest, NextResponse} from 'next/server';
+import mongoose from 'mongoose';
 import ProductModel from '@/models/Products';
-import { db } from '@/database';
-import { pipeline } from 'stream';
-import { promisify } from 'util';
-import { Readable } from 'stream';
-
-const pipelineAsync = promisify(pipeline);
+import {db} from '@/database';
+import {IProduct} from '@/interfaces/IProduct';
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
     await db.connect();
 
     try {
-        const formData = await request.formData();
-        const title = formData.get('title');
-        const description = formData.get('description');
-        const price = formData.get('price');
-        const stock = formData.get('stock');
-        const imageFile = formData.get('image');
-        const categoryId = formData.get('categoryId');
+        const productData: IProduct = await request.json();
 
-        if (
-            typeof title !== 'string' ||
-            typeof description !== 'string' ||
-            typeof price !== 'string' ||
-            typeof stock !== 'string' ||
-            typeof categoryId !== 'string' ||
-            !(imageFile instanceof Blob) // Cambia File a Blob si no estás en un entorno de navegador
-        ) {
-            throw new Error('Invalid form data');
+        if (!mongoose.isValidObjectId(productData.categoryId)) {
+            return new NextResponse(JSON.stringify({
+                error: 'Invalid categoryId'
+            }), {
+                status: 400,
+                headers: {'Content-Type': 'application/json'}
+            });
         }
 
-        // Crea el directorio si no existe
-        const uploadDir = path.join(process.cwd(), 'public/gamebazar');
-        await fsPromises.mkdir(uploadDir, { recursive: true });
-
-        const imageFilePath = path.join(uploadDir, imageFile.name);
-
-        // Guarda la imagen en el sistema de archivos
-        const imageFileStream = imageFile.stream();
-        const reader = imageFileStream.getReader();
-
-        const nodeStream = new Readable({
-            async read() {
-                const result = await reader.read();
-                if (result.done) {
-                    this.push(null);
-                } else {
-                    this.push(new Uint8Array(result.value));
-                }
-            }
-        });
-
-        const fileStream = createWriteStream(imageFilePath);
-        await pipelineAsync(nodeStream, fileStream);
-
-        const newProduct = new ProductModel({
-            title,
-            description,
-            price: parseFloat(price),
-            stock: parseInt(stock),
-            image: `/gamebazar/${imageFile.name}`,
-            categoryId: parseInt(categoryId)
-        });
-
+        const newProduct = new ProductModel(productData);
         const savedProduct = await newProduct.save();
 
         return new NextResponse(JSON.stringify(savedProduct), {
@@ -79,7 +34,7 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
         if (error instanceof Error) {
             errorMessage = error.message;
         }
-        return new NextResponse(JSON.stringify({ error: errorMessage }), {
+        return new NextResponse(JSON.stringify({error: errorMessage}), {
             status: 500,
             headers: {
                 'Content-Type': 'application/json'
