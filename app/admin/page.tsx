b5f1900576094dc5accac0f53dae2bb954cadd99
Leandro Rey <leandrodrey@gmail.com>
2023-12-19 03:36:20 -0300

    Remove Header component from admin page
    The Header component was deleted from the admin page in the code. This change simplifies the page rendering by reducing the number of components and streamlining the content of the administration interface.

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,6 @@
 import {ReactElement} from 'react';
 import {IProduct} from "@/interfaces/IProduct";
 import {getProductByCategory} from "@/services/ProductService";
-import Header from '@/app/admin/components/Header';
 import MainContent from '@/app/admin/components/MainContent';
 
 export const dynamic = 'force-dynamic';
@@ -12,7 +11,6 @@ export default async function AdminPage(): Promise<ReactElement> {
 
     return (
         <>
-            <Header/>
             <div className="flex-1 flex flex-col">
                 <MainContent products={allProducts}/>
             </div>
