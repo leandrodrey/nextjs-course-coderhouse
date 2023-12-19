c01a864bdeec7d7aa9f723d1e06444403f05e29d
Leandro Rey <leandrodrey@gmail.com>
2023-12-19 03:36:13 -0300

    Replace ProductsTableContainer with ProductsCartTable in cart page
    The cart page was previously using the ProductsTableContainer. This changes replaces it with ProductsCartTable which is more suited for the functionality of the cart page. This better aligns with the page's specific needs.

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,5 +1,5 @@
 import {ReactElement} from "react";
-import ProductsTableContainer from "@/components/ui/ProductsTableContainer";
+import ProductsCartTable from "@/components/ui/ProductsCartTable";
 
 export const dynamic = 'force-dynamic';
 
@@ -8,7 +8,7 @@ export default function Cart(): ReactElement {
     return (
         <>
             <h1 className='mb-5 text-3xl'>Shopping Cart</h1>
-            <ProductsTableContainer action='remove' context='cart' />
+            <ProductsCartTable/>
         </>
     )
 }
