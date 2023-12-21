4df7160126c46e1ac0c55e44e51547cf0f5c6f84
Leandro Rey <leandrodrey@gmail.com>
2023-12-21 19:08:16 -0300

    Update loading spinner color in UI
    Changed the color of the spinning loading icon to blue from an existing gray. This was done in the Loader component, enhancing the visual aesthetics of the loader.

--- components/ui/Loader.tsx
+++ components/ui/Loader.tsx
@@ -3,7 +3,7 @@ import {FC} from "react";
 const Loader: FC = () => {
     return (
         <div className="flex justify-center items-center h-screen">
-            <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+            <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-blue-300"></div>
         </div>
     );
 };
