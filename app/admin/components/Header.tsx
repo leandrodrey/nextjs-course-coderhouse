d6653aa0d2d7244c5696a3ced1ed7c76fbb12041
Leandro Rey <leandrodrey@gmail.com>
2023-12-21 19:07:35 -0300

    Replace 'next/image' import with 'next-cloudinary'
    Switched from using 'next/image' to 'next-cloudinary' in both 'EmptyCart.tsx' and 'Header.tsx'. This helps to better manage image rendering from the cloud and improves performance.

--- app/admin/components/Header.tsx
+++ app/admin/components/Header.tsx
@@ -1,6 +1,5 @@
 import {FC} from "react";
-import Image from "next/image";
-import {DocumentMagnifyingGlassIcon} from "@heroicons/react/24/solid";
+import { CldImage } from "next-cloudinary";
 
 interface HeaderProps {
 }
@@ -9,18 +8,10 @@ const Header: FC<HeaderProps> = () => {
 
     return (
         <header className="bg-stone-900 shadow-sm">
-            <div className="flex justify-end md:justify-between items-center py-2 pr-2 md:pr-0 md:px-4">
-                <div className="items-center hidden md:flex">
-                    <DocumentMagnifyingGlassIcon className="h-6 w-6" />
-                    <input
-                        type="search"
-                        className="block w-full pl-10 pr-3 py-2 ml-4 text-gray-700 bg-gray-100 border border-transparent rounded-md focus:border-blue-500 focus:bg-white focus:ring-0"
-                        placeholder="Search..."
-                    />
-                </div>
+            <div className="flex justify-end items-center py-2 pr-2 md:pr-0 md:px-4">
                 <div className="flex items-center">
                     <h1 className="mr-3">Bienvenido, <span className="text-blue-300 font-semibold">Admin</span></h1>
-                    <Image
+                    <CldImage
                         src="/gamebazar/user_avatar.png"
                         alt="User Avatar"
                         width={100}
--- components/ui/EmptyCart.tsx
+++ components/ui/EmptyCart.tsx
@@ -1,6 +1,6 @@
 import {FC} from "react";
 import Link from "next/link";
-import Image from "next/image";
+import { CldImage } from "next-cloudinary";
 
 const EmptyCart: FC = () => {
 
@@ -8,8 +8,8 @@ const EmptyCart: FC = () => {
         <>
             <div className="flex flex-col items-center justify-center">
                 <h1 className="text-3xl">There are no products in the cart!</h1>
-                <Image
-                    src="/gamebazar/ybhx45zwsibzbkvjr6nc.png"
+                <CldImage
+                    src="/gamebazar/ybhx45zwsibzbkvjr6nc"
                     alt='There are no products in the cart!'
                     width={400}
                     height={400}
