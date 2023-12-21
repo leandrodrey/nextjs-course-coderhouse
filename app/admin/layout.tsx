233714ecb9abb627a649a48776e94e36f9ebb779
Leandro Rey <leandrodrey@gmail.com>
2023-12-21 19:08:24 -0300

    Reorder imports and add padding to child elements in DashboardLayout
    The DashboardLayout component has been updated to properly order the import statements and add padding to its child elements. This enhances the visual presentation of child elements within the layout by providing them with more space, and improves the code's readability by organizing the imports.

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,28 +1,30 @@
 "use client"
 import {ReactNode} from "react";
-import {redirect} from "next/navigation";
-import {signOut, useSession} from "next-auth/react";
 import Header from "@/app/admin/components/Header";
+import {useSession} from "next-auth/react";
+import {redirect} from "next/navigation";
 
-export default function DashboardLayout({ children }: {
+export default function DashboardLayout({children}: {
     children: ReactNode
 }) {
 
-/*    const {data: session, status} = useSession();
+    const {data: session, status} = useSession();
 
     if (!session) redirect('api/auth/signin');
 
-    if (session) {*/
+    if (session) {
         return (
             <div className="container mx-auto mt-10 md:p-5">
                 {/*<p>Bienvenido, {session.user?.email}</p>*/}
                 {/*<button onClick={() => signOut()}>Logout</button>*/}
                 <div className="flex flex-col h-full flex-1">
                     <Header/>
-                    {children}
+                    <div className="shadow rounded-lg p-6">
+                        {children}
+                    </div>
                 </div>
             </div>
         );
-    /* }*/
+    }
 
 }
