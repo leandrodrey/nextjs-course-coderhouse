d88afd4ca12091b0e5c17485bbbc1be407c12105
Leandro Rey <leandrodrey@gmail.com>
2023-12-22 14:03:58 -0300

    Update Header component to use dynamic user data
    This commit updates the Header to use next-auth to pull in dynamic user data. It also includes a new interface for the User data retrieved. If there is no image for the user, it will default to a preset avatar. Changes enhance the personalization aspect of the user experience.

--- app/admin/components/Header.tsx
+++ app/admin/components/Header.tsx
@@ -1,23 +1,32 @@
 import {FC} from "react";
-import { CldImage } from "next-cloudinary";
+import Image from "next/image";
+import {useSession} from "next-auth/react";
+import {IUser} from "@/interfaces/IUser";
 
-interface HeaderProps {
-}
+const Header: FC = () => {
 
-const Header: FC<HeaderProps> = () => {
+    const session = useSession();
+    const user = session.data?.user as IUser;
+    const userImage = user?.image || '/gamebazar/user_avatar.png';
 
     return (
         <header className="bg-stone-900 shadow-sm">
             <div className="flex justify-end items-center py-2 pr-2 md:pr-0 md:px-4">
                 <div className="flex items-center">
-                    <h1 className="mr-3">Bienvenido, <span className="text-blue-300 font-semibold">Admin</span></h1>
-                    <CldImage
-                        src="/gamebazar/user_avatar.png"
-                        alt="User Avatar"
-                        width={100}
-                        height={100}
-                        className="object-contain rounded-full shadow-md"
-                    />
+                    {session.data && (
+                        <>
+                            <h2 className="mr-3">Bienvenido, <span className="text-blue-300 font-semibold">{session.data?.user?.name}</span></h2>
+                            <Image
+                                src={userImage}
+                                alt="User Avatar"
+                                width={100}
+                                height={100}
+                                className="object-contain rounded-full shadow-md"
+                                placeholder="blur"
+                                blurDataURL="/gamebazar/user_avatar.png"
+                            />
+                        </>
+                    )}
                 </div>
             </div>
         </header>
