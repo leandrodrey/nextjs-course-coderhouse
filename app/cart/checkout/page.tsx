970eddb6c299a31b0bb9a399c2182f18318fba81
Leandro Rey <leandrodrey@gmail.com>
2023-12-28 14:28:46 -0300

    Refactor CSS for responsive cart display
    The CSS classes controlling the display of the product cart table in the checkout page have been adjusted. These changes ensure the layout is responsive, appropriately adjusting for both small and large screen sizes. The product cart table will now be consistently visible across all device sizes.

--- app/cart/checkout/page.tsx
+++ app/cart/checkout/page.tsx
@@ -8,8 +8,8 @@ export default function CheckOutPage(): ReactElement {
         <>
             <h1 className='mb-5 text-3xl'>Checkout</h1>
             <p className='mb-5'>Please fill in the form below to complete your order.</p>
-            <div className="shadow-md sm:rounded-lg flex justify-between">
-                <div className="overflow-y-auto w-[30%]">
+            <div className="shadow-md sm:rounded-lg flex flex-col md:flex-row justify-between">
+                <div className="overflow-y-auto md:w-[30%]">
                     <CartPreview/>
                 </div>
                 <div className="bg-stone-900 m-3 p-2 rounded-lg shadow-lg grow">
